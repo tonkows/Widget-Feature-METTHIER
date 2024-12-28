438329e79b89ab408410362929baf544e9daa07f
Kanittha.s <s.kanittha2002@gmail.com>
2024-12-28 13:45:12 +0700

    add text

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
         <div className="main-layout">
           {/* Sidebar */}
           <Sidebar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
-
+        <p>hallo</p>
           {/* Main Content */}
           <div className={`main-content ${isCollapsed ? "collapsed" : ""}`}>
             <div className="grid-container">
